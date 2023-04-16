cf674c930635119521feecda45071078b6f63916
Silvina Fernanda Acosta <silvinaferacosta@gmail.com>
2023-04-16 18:28:05 -0300

    Update Silvina - Maven

---
+++ src/main/java/com/proyecto/clases/Partido.java
@@ -0,0 +1,67 @@
+package com.proyecto.clases;
+
+public class Partido {
+    // Atributos de la clase
+    private Equipo equipo1;
+    private Equipo equipo2;
+    private int golesEquipo1;
+    private int golesEquipo2;
+
+    // Método Constructor
+    public Partido(Equipo equipo1, Equipo equipo2, int golesEquipo1, int golesEquipo2) {
+        this.equipo1 = equipo1;
+        this.equipo2 = equipo2;
+        this.golesEquipo1 = golesEquipo1;
+        this.golesEquipo2 = golesEquipo2;
+    }
+
+    // Este método devuelve el resultado del partido para un equipo dado
+    public ResultadoEnum resultado(Equipo equipo){
+        if (equipo==equipo1){
+            if (golesEquipo1==golesEquipo2){
+                return ResultadoEnum.empate;
+            }else if(golesEquipo1>golesEquipo2){
+                return ResultadoEnum.ganador;
+            }else{
+                return ResultadoEnum.perdedor;
+            }
+        } else if (equipo==equipo2) {
+            if (golesEquipo2==golesEquipo1){
+                return ResultadoEnum.empate;
+            }else if(golesEquipo2>golesEquipo1){
+                return ResultadoEnum.ganador;
+            }else{
+                return ResultadoEnum.perdedor;
+            }
+        }else{
+            return null;
+        }
+    }
+
+    //Métodos Getters y Setters
+    public Equipo getEquipo1() {
+        return equipo1;
+    }
+    public void setEquipo1(Equipo equipo1) {
+        this.equipo1 = equipo1;
+    }
+    public Equipo getEquipo2() {
+        return equipo2;
+    }
+    public void setEquipo2(Equipo equipo2) {
+        this.equipo2 = equipo2;
+    }
+    public int getGolesEquipo1() {
+        return golesEquipo1;
+    }
+    public void setGolesEquipo1(int golesEquipo1) {
+        this.golesEquipo1 = golesEquipo1;
+    }
+    public int getGolesEquipo2() {
+        return golesEquipo2;
+    }
+    public void setGolesEquipo2(int golesEquipo2) {
+        this.golesEquipo2 = golesEquipo2;
+    }
+
+}
---
+++ src/main/java/com/proyecto/clases/ResultadoEnum.java
@@ -0,0 +1,5 @@
+package com.proyecto.clases;
+
+public enum ResultadoEnum {
+    ganador, perdedor, empate;
+}
