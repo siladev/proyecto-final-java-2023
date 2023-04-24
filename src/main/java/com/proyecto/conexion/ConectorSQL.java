c6f665f80f502eeee8085b6557e009e9c7c6f36b
silvinaferacosta <silvinaferacosta@gmail.com>
2023-04-24 00:25:01 -0300

    Create ConectorSQL.java Entrega 3

---
+++ src/main/java/com/proyecto/conexion/ConectorSQL.java
@@ -0,0 +1,10 @@
+package com.proyecto.conexion;
+
+public class ConectorSQL {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/tp_pronosticos_deportivos";
+
+    // Credenciales credentials
+    public static final String USER = "root";
+    public static final String PASS = "root";
+}
